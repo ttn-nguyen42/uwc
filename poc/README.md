7904180b0be379a778325836736e3397b4143981
ttn.nguyen42 <ttn.nguyen42@gmail.com>
2022-11-13 22:42:27 +0700

    added documents and pocs

---
+++ poc/README.md
@@ -0,0 +1,3 @@
+# Deployment
+
+N/A
--- src/routes/overview/Overview.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@mui/material";
-import useAuth from "../../utils/hooks/UseAuth";
-
-export default function Overview() {
-    const auth = useAuth();
-    return <div><Button variant="outlined" onClick={() => {
-        auth.logout();
-    }}>Logout</Button></div>
-}
